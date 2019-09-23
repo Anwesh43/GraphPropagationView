5ae7cc5510f6fffca00e46d2c4202782f410d246
Anwesh43 <anweshthecool0@gmail.com>
2019-09-24 03:06:32 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/graphpropagationanimation/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/graphpropagationanimation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.graphpropagationanimation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.graphpropagationview.GraphPropagationView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         GraphPropagationView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
