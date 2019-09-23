8188a541cfbd8daec488d23d0d8cca86232d68bd
Anwesh43 <anweshthecool0@gmail.com>
2019-09-23 11:31:57 +0530

    adding constants

--- graphpropagationview/src/main/java/com/anwesh/uiprojects/graphpropagationview/GraphPropagationView.kt
+++ graphpropagationview/src/main/java/com/anwesh/uiprojects/graphpropagationview/GraphPropagationView.kt
@@ -3,3 +3,17 @@ package com.anwesh.uiprojects.graphpropagationview
 /**
  * Created by anweshmishra on 23/09/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val scGap : Float = 0.01f
+val strokeFactor : Float = 90f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
