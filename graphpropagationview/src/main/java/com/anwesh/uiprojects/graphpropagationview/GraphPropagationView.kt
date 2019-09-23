928c6cb51d61aba448ff09af95d741e6d8c9d0ed
Anwesh43 <anweshthecool0@gmail.com>
2019-09-24 01:49:35 +0530

    adding Canvas extension methods

--- graphpropagationview/src/main/java/com/anwesh/uiprojects/graphpropagationview/GraphPropagationView.kt
+++ graphpropagationview/src/main/java/com/anwesh/uiprojects/graphpropagationview/GraphPropagationView.kt
@@ -17,7 +17,20 @@ val scGap : Float = 0.01f
 val strokeFactor : Float = 90f
 val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val sizeFactor : Float = 10f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawRect(x : Float, y : Float, size : Float, sc : Float, paint : Paint) {
+    val sizeSc : Float = size * sc
+    save()
+    translate(x, y)
+    drawRect(RectF(-sizeSc, -sizeSc, sizeSc, sizeSc), paint)
+    restore()
+}
+
+fun Canvas.drawLinesToNeighbor(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    drawLine(x1, y1, x2, y2, paint)
+}
